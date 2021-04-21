9cb4dbd7a4ca51b5d021bb54cc3a58f77907233c
Stepan Suvorov <stevermeister@gmail.com>
2021-04-21 10:26:33 +0200

    +worker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, VERSION } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
-  `,
-  styles: []
+    <button (click)="randomNumber()">random</button><br />
+    <button (click)="findBigPrime()">hard calculations</button><br />
+    <button (click)="findBigPrimeWorker()">
+      same hard calculations with Worker</button
+    ><br />
+    {{ output1 }}<br />
+    {{ output2 }}<br />
+  `
 })
 export class AppComponent {
-  title = 'webworker-base';
+  output1: string = '';
+  output2: string = '';
+  worker: Worker|undefined;
+
+  constructor() {
+    if (typeof Worker !== 'undefined') {
+      this.worker = new Worker('./app.worker', { type: 'module' });
+      this.worker.onmessage = ({ data }) => {
+        this.output2 = data;
+      };
+    } else {
+      console.log('Web Workers are not supported in this environment.');
+    }
+  }
+
+  randomNumber() {
+    this.output1 = Math.random().toString();
+  }
+
+  findBigPrime() {
+    this.output2 = find_big_prime().toString();
+  }
+
+  findBigPrimeWorker() {
+    this.worker?.postMessage("John");
+  }
+}
+
+function find_big_prime() {
+  let start = Math.floor(Math.random() * 1000000000);
+  let is_prime = false;
+  while (!is_prime) {
+    is_prime = test_prime(start);
+    start++;
+  }
+  return start;
+}
+
+function test_prime(n:number) {
+  if (n === 1) {
+    return false;
+  } else if (n === 2) {
+    return true;
+  } else {
+    for (var x = 2; x < n; x++) {
+      if (n % x === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
